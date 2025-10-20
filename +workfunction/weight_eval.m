f3916465c75f07235837baf06742a8e820ef4d7b
rickymoschini <riky.moschini@gmail.com>
2025-10-20 18:57:56 +0200

    Update weight_eval.m

--- +workfunction/weight_eval.m
+++ +workfunction/weight_eval.m
@@ -7,7 +7,7 @@ Q_impennaggi=3.5*(1/QM_S)*Q*data.g; %[N]
 Q_carrello=0.03*Q; %[kg]
 Q_impianti=0.2*Q; %[kg]
 Q_motore=0.2*T0_S*(1/QM_S)*Q; %[kg]
-Q_fisso=(data.p_pax+data.W_liqu+data.p_sed); %[kg]
+Q_fisso=(data.p_pax+data.p_sed)*(data.n_pax+data.n_eq)+data.W_liqu; %[kg]  %%CONTROLLARE
 Q_f=k*Q;
 QM=Q_ala+Q_fus+Q_impennaggi+Q_carrello+Q_impianti+Q_motore+Q_fisso+Q_f;
 end
